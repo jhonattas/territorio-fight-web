ebd3c59aae59aa01c3d694a0ba18acd4ffcbbb7e
Jhonattas Henrique Goncalves <jhonattas@soucriador.com>
2021-03-13 11:28:48 -0300

    tema temporario

--- index.php
+++ index.php
@@ -1,11 +1,319 @@
-<?php 
+<!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
 
-  require __DIR__ . "/vendor/autoload.php";
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  use CoffeeCode\Router\Router;
 
-  $router = new Router(URL_BASE);
-  
-  require __DIR__ . "/source/App/Routes.php";
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>ONITRANSP</title>
 
-  $router->dispatch();
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="assets/css/theme.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand d-flex align-items-center fw-bolder fs-2" href="#">
+            <div class="text-info" style="color: #003c65 !important;">ONITRAN</div>
+            <div class="text-warning" style="color: #6da9d5 !important;">SP</div>
+          </a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto pt-2 pt-lg-0">
+              <!--
+              <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item px-2"><a class="nav-link fw-medium" href="#features">Jobs</a></li>
+              <li class="nav-item px-2"><a class="nav-link fw-medium" href="#pricing">Career tips</a></li>
+            -->
+            </ul>
+            <form class="ps-lg-5">
+              <a 
+                class="btn btn-lg btn-primary rounded-pill order-0"
+                style="background-color: #075e54; border: 1px solid #075e54;"
+                href="https://api.whatsapp.com/send?phone=5519999689812"
+                target="_new">
+                <i class="fab fa-whatsapp"></i> Contato</a>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <section class="py-0" id="home">
+        <div class="bg-holder d-none d-sm-block" style="background-image:url(assets/img/illustrations/hero-bg.png);background-position:right top;background-size:contain;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="bg-holder d-sm-none" style="background-image:url(assets/img/illustrations/hero-bg.png);background-position:right top;background-size:contain;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row align-items-center min-vh-75 min-vh-md-100">
+            <div class="col-md-7 col-lg-6 py-6 text-sm-start text-center">
+              <h1 class="mt-6 mb-sm-4 display-2 fw-semi-bold lh-sm fs-4 fs-lg-6 fs-xxl-8">Transporte <br class="d-block d-lg-none d-xl-block" />refrigerado</h1>
+              <p class="mb-4 fs-1">Transportes de produtos <u>climatizados</u>, <u>refrigerados</u>, e <u>congelados</u></p>
+              <div class="pt-3">
+                <a 
+                class="btn btn-lg btn-primary rounded-pill order-0"
+                style="background-color: #075e54; border: 1px solid #075e54;"
+                href="https://api.whatsapp.com/send?phone=5519999689812"
+                target="_new">
+                <i class="fab fa-whatsapp"></i> Contato</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+     
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+      <section class="py-8">
+        <div class="container-lg">
+          <div class="row flex-center">
+            <div class="col-md-11 col-lg-6 col-xl-4 col-xxl-5">
+              <h6 class="fs-3 fs-lg-4 lh-sm">Serviços</h6>
+            </div>
+            <div class="col-lg-4 position-relative mt-n5 mt-md-n4"><a class="carousel-control-prev carousel-icon z-index-2" href="#carouselExampleDark" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next carousel-icon z-index-2" href="#carouselExampleDark" role="button" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+          </div>
+          <div class="row flex-center">
+            <div class="col-xl-10 px-0">
+              <div class="carousel slide pt-6" id="carouselExampleDark" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="10000">
+                    <div class="row h-100 m-lg-7 mx-3 mt-6 mx-md-4 my-md-7">
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-lg">
+                          <div class="card-span-img"><img src="assets/img/gallery/user-1.png" alt="" /></div>
+                          <div class="card-body d-flex flex-column flex-center py-6">
+                            <p class="card-text text-center text-1000 px-4">Entrega rápida e dentro do prazo combinado</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-lg">
+                          <div class="card-span-img"><img src="assets/img/gallery/user-2.png" alt="" /></div>
+                          <div class="card-body d-flex flex-column flex-center py-6">
+                            <p class="card-text text-center text-1000 px-4">Temperatura monitorada com precisão</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-lg">
+                          <div class="card-span-img"><img src="assets/img/gallery/user-3.png" alt="" /></div>
+                          <div class="card-body d-flex flex-column flex-center py-6">
+                            <p class="card-text text-center text-1000 px-4">Coletas e entregas agendadas</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item" data-bs-interval="2000">
+                    <div class="row h-100 m-lg-7 mx-3 mt-6 mx-md-4 my-md-7">
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-lg">
+                          <div class="card-span-img"><img src="assets/img/gallery/user-1.png" alt="" /></div>
+                          <div class="card-body d-flex flex-column flex-center py-6">
+                            <p class="card-text text-center text-1000 px-4">Fretes dedicados para sua empresa ou produtos</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-lg">
+                          <div class="card-span-img"><img src="assets/img/gallery/user-2.png" alt="" /></div>
+                          <div class="card-body d-flex flex-column flex-center py-6">
+                            
+                            <p class="card-text text-center text-1000 px-4">Confiabilidade total</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="py-5">
+        <div class="bg-holder d-none d-sm-block" style="background-image:url(assets/img/illustrations/category-bg.png);background-position:right top;background-size:200px 320px;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-6 text-center mb-3">
+              <h5 class="fw-bold fs-3 fs-lg-5 lh-sm mb-3">Produtos Transportados</h5>
+              <p class="mb-5">alguns dos produtos que transportamos</p>
+            </div>
+          </div>
+          <div class="row flex-center h-100">
+            <div class="col-10 col-xl-9">
+              <div class="row">
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/01.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/02.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/03.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/04.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/05.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 pb-lg-6 px-lg-4 pb-4">
+                  <div 
+                    class="card py-4 shadow-sm h-100 hover-top" 
+                    style="
+                      background-image: url('http://onitransp.com.br/images/products/06.jpg');
+                      background-size: cover;
+                      background-position: center center;
+                      background-repeat: no-repeat;
+                      min-height: 313px;">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+
+      <section class="py-0"><img class="w-100" src="assets/img/illustrations/come-on-join.png" alt="" />
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-12 text-center">
+              <h6 class="fw-bold fs-3 fs-lg-5 lh-sm">Conte conosco !</h6>
+              <p>Nossa sede fica localizada em <b>Campinas - SP</b>, mas atendemos cidades da região. </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      
+      <section class="py-0 bg-primary-gradient">
+        <div class="bg-holder" style="background-image:url(assets/img/illustrations/footer-bg.png);background-position:center;background-size:cover;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          
+          
+          <div class="row flex-center">
+            <div class="col-auto my-4">
+              <ul class="list-unstyled list-inline">
+              
+                <li class="list-inline-item me-3"><a class="text-decoration-none" href="#!">
+                    <svg class="bi bi-facebook" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#1F3A63" viewBox="0 0 16 16">
+                      <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                    </svg></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto mb-2">
+              <p class="mb-0 fs--1 text-white my-2 text-center">&copy; Criado com
+                <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1F3A63" viewBox="0 0 16 16">
+                  <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
+                </svg>&nbsp;por&nbsp;<a class="text-white" href="https://soucriador.com/" target="_blank">Sou Criador </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="vendors/@popperjs/popper.min.js"></script>
+    <script src="vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="vendors/is/is.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
